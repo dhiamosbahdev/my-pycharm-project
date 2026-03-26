9fc5610d0897ec67e1c7c192b26f2bdd5d6e0978
dhiamosbahdev <dhiamosbahdev@gmail.com>
2026-03-26 12:25:23 +0100

    Ajout: Page 404

--- bibliotheque/settings.py
+++ bibliotheque/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j-v*ajw4vmrb0v8d3$jzzp2)$m5bnxyb^xiik+-)&l239-o$*p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
