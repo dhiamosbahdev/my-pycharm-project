602806f8a9cc5ccb359fc6d626e1fe3734c7d357
dhiamosbahdev <dhiamosbahdev@gmail.com>
2026-03-27 09:20:44 +0100

    Ajout: Decorateur

--- gestionlivre/urls.py
+++ gestionlivre/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from gestionlivre.views import livreviewlist, livredetail, recherche
 
 urlpatterns = [
     path('', livreviewlist.as_view(), name='livre_list'),
-    path('<int:pk>/', livredetail.as_view(), name='livre_detail'),
+    path('<int:pk>/', login_required(livredetail.as_view()), name='livre_detail'),
     path('search', recherche.as_view(), name='search')
 
 ]
